8bec74c250e7e2a735abe8dc7178a6ee95c344db
tdevm <cr14b17@gmail.com>
2020-03-01 16:39:35 +0530

    initialize express and sequelize

---
+++ config/config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    "development": {
+        "username": process.env.USERNAME,
+        "host": process.env.HOST,
+        "dialect": process.env.DIALECT,
+    },
+    "test": {
+        "username": process.env.USERNAME,
+        "host": process.env.HOST,
+        "dialect": process.env.DIALECT,
+    },
+    "production": {
+        "username": process.env.USERNAME,
+        "host": process.env.HOST,
+        "dialect": process.env.DIALECT,
+    }
+};
+
